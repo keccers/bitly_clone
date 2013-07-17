2cce0f5ce4d398fe488052061194e3a03980d293
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-17 10:16:29 -0500

    Set up proper post route & form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,19 @@
 get '/' do
   # Look in app/views/index.erb
-  puts request.inspect
+  # @errors = errors[:attribute] ||= ""
+  # puts request.inspect
   erb :index
 end
 
 post '/urls' do
-  @url = Url.new(params[url])
-  @url.valid?
-  @errors = errors[:attribute]
+  @url = Url.new(params[:url])
+  if @url.valid?
+    @url.save
+    @message = @url.short 
+  else
+    # @errors = errors[:attribute]
+  end
+  erb :index
 end
 
 # e.g., /q6bda
