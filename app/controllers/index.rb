cd0009efc982bea24abc26a4a0852f57f8a6cdf6
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-17 09:43:40 -0500

    Update url table schema, start outlining url shortener post method

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,16 @@
 get '/' do
   # Look in app/views/index.erb
+  puts request.inspect
   erb :index
+end
+
+post '/urls' do
+  @url = Url.new(params[url])
+  @url.valid?
+  @errors = errors[:attribute]
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  # redirect to appropriate "long" URL
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,9 @@
 class Url < ActiveRecord::Base
   validates :long, format: {with: /^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix, message: "Not a valid URL."}
   validates :short, uniqueness: true
+
+  def self.append_shorty
+    
+  end
+
 end
