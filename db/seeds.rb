38b8478ee5fe04c1aa87332b5c45c435abc9f8a0
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-17 08:54:02 -0500

    Start writing seed file

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Url.create(long: "http://xkcd.com", short: "shorty.com/xkcd")
+Url.create(long: "http://xojane.com", short: "shorty.com/xoj")
+Url.create(long: "http://valleywag.gawker.com", short: "shorty.com/filth")
+Url.create(long: "http://xkcd.com", short: "shorty.com/xkcd")
